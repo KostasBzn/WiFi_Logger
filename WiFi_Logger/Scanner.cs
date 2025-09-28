b5edba11a9275d198c8d52368e015e931f26a3ae
Kostas Bouzianis <kostasbouzianis@gmail.com>
2025-09-28 10:48:42 +0100

    basic_structure

---
+++ WiFi_Logger/Scanner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiFi_Logger
+{
+    internal class Scanner
+    {
+        public void Start()
+        {
+            Net_Scan();
+        }
+
+        private void Net_Scan()
+        {
+
+        }
+
+        private void SaveToFile(string output)
+        {
+
+        }
+    }
+}
