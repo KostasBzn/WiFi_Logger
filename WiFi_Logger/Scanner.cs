0e1347e2c81d83142bf6be0f3caa0cf775890df9
Kostas Bouzianis <kostasbouzianis@gmail.com>
2025-09-28 18:51:32 +0100

    Update_loc_services_activation
    updated registry check, auto enable when running as admin

--- WiFi_Logger/Scanner.cs
+++ WiFi_Logger/Scanner.cs
@@ -3,17 +3,54 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Win32;
 
 namespace WiFi_Logger
 {
     internal class Scanner
-    {
+    {  
         public void Start()
         {
-            Net_Scan();
-        }
+            try
+            {
+                string keyPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location";
+                string valueName = "Value";
+                string lmVal = Registry.LocalMachine.OpenSubKey(keyPath)?.GetValue(valueName)?.ToString();
+                string cuVal = Registry.CurrentUser.OpenSubKey(keyPath)?.GetValue(valueName)?.ToString();
+                bool locServOn = "Allow".Equals(lmVal, StringComparison.OrdinalIgnoreCase) &&
+                          "Allow".Equals(cuVal, StringComparison.OrdinalIgnoreCase);
+
+                if (!locServOn)
+                {
+                    if (IsAdmin())
+                    {
+                        if (!"Allow".Equals(lmVal, StringComparison.OrdinalIgnoreCase))
+                            Registry.LocalMachine.CreateSubKey(keyPath).SetValue(valueName, "Allow", RegistryValueKind.String);
+
+                        if (!"Allow".Equals(cuVal, StringComparison.OrdinalIgnoreCase))
+                            Registry.CurrentUser.CreateSubKey(keyPath).SetValue(valueName, "Allow", RegistryValueKind.String);
+                    }
+                    else
+                    {
+                        SaveToFile("Location services are disabled, run as admin to enable and scan.");
+                        return;
+                    }
+                }
+                Debug.WriteLine("Running NetScan", "Status");
+                Net_Scan();
+            }
+
+            catch (Exception ex) 
+            {
+                string er = "--- ERROR  ---\n" + ex.Message + "\n";
+                SaveToFile(er);
+            }
+            
+        } 
 
         private void Net_Scan()
         {
@@ -56,6 +93,11 @@ namespace WiFi_Logger
             {
                 sw.WriteLine(output);
             }
+        }
+
+        public static bool IsAdmin()
+        {
+            return new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
         }
     }
 }
