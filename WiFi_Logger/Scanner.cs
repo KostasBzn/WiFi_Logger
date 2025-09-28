904fa393091145edfcb62ddc40c79b9bfedfacc2
Kostas Bouzianis <kostasbouzianis@gmail.com>
2025-09-28 10:59:19 +0100

    scan ok

--- WiFi_Logger/Scanner.cs
+++ WiFi_Logger/Scanner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,31 @@ namespace WiFi_Logger
 
         private void Net_Scan()
         {
+            try
+            {
+                Process proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "netsh",
+                        Arguments = "wlan show networks mode=bssid",
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        StandardOutputEncoding = Encoding.UTF8,
+                    }
+                };
+                proc.Start();
+                string output = proc.StandardOutput.ReadToEnd();
+                Debug.WriteLine("scan result " + output);
+                proc.WaitForExit();
+
+            }
+            catch (Exception ex) 
+            {
+                Debug.WriteLine("Net_Scan failed: " + ex.Message);
+            }
 
         }
 
